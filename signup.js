4eec7a9d4980620e9b19e9071884447e7d37cd9b
VIGNESH P <161745974+vignesh8691@users.noreply.github.com>
2025-06-29 21:31:21 -0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,18 @@
+const uid = "abc123";
+
+var fullName = "Vignesh"
+var email = "pvignesh2026@gmail.com"
+var password = "1234567890"
+var confirm = "1234567890"
+var courseCount = 1;
+var isLoggedinfromgoogle = false
+
+// console.log(fullName)
+
+console.log(`
+    With unique id: ${uid}
+    User is : ${fullName}
+    and his email is ${email}
+    and uses the password: ${password}
+
+    `)

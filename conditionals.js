4d4e6823aedcf3085eb6e27092236b99286e8861
VIGNESH P <161745974+vignesh8691@users.noreply.github.com>
2025-06-29 21:30:18 -0700

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+var temperature;
+temperature = 25;
+
+
+if (temperature < 20) {
+    console.log("It's very cold");
+}
+else if (temperature < 30) {
+    console.log("It's moderate");
+}
+else {
+    console.log("It's Hot!");
+}
